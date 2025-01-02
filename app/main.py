d4fd83794d5db493e76536ed936203ed0176a9ce
Stanislauf <psv88877@gmail.com>
2025-01-02 17:46:47 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
+
+
 from fastapi import FastAPI
 from app.routers import task, user
+from app.backend.db import engine, Base
+from app.models.user import User
+from app.models.task import Task
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    Base.metadata.create_all(bind=engine) # Создаем таблицы в базе данных
 
 @app.get("/")
 async def root():
